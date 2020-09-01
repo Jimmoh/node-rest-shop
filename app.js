d71da4cbf4c18ebae7a780f4614778bbba564ea8
Jimmoh <jkjimmoh@gmail.com>
2020-09-01 23:21:01 +0300

    Error handling

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
