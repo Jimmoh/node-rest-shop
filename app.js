c25a22cf49a6c4ff0a91b2b625d3937b95c48046
Jimmoh <jkjimmoh@gmail.com>
2020-09-02 15:23:59 +0300

    Handling CORS

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ const bodyParser = require('body-parser');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, PUT')
+        return res.status(200).json({});
+    }
+    next();
+});
 
+//Routes to handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
