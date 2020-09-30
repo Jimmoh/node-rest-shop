7452387b93bace8de475fc89a19d1794c17a8911
Jimmoh <jkjimmoh@gmail.com>
2020-09-30 17:56:31 +0300

    Managing Orders with Mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,22 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
 
 //Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders have been fetched'
-    });
+    Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost3000/orders/' + doc._id
+                    }
+                }
+            })
+           
+            
+        });
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error: err
+        });
+    })
 });
 
 //Handle incoming POST requests to /orders
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Order has been created',
-        yourOrder: order
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order
+    .save()
+    .then(result =>{
+        console.log(result);
+        res.status(201).json({
+            message: 'Order Saved!',
+            request: {
+                type: 'POST'
+                   }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
